620b8a775a4f39d9f662fda9603d11230784ced2
LuanMobile <luanhenriique.ls13@gmail.com>
2024-04-15 13:18:08 -0300

    refactoring completed

--- src/Models/Database.php
+++ src/Models/Database.php
@@ -8,7 +8,7 @@ class Database
 {
     public static function getConnection()
     {
-        $pdo = new PDO("pgsql:host=localhost;port=5432;dbname=db_test", "postgres", "psswd", [
+        $pdo = new PDO("pgsql:host=localhost;port=5432;dbname=postgres", "postgres", "psswd", [
             PDO::ATTR_DEFAULT_FETCH_MODE => PDO::FETCH_ASSOC
         ]);
 
